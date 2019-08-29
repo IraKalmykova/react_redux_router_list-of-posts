d2e2eec6249cd8f80c1f0c53fca913ab5dc494e5
Ira <kalmykova_ira@ukr.net>
2019-08-29 13:01:38 +0300

    Updates

---
+++ precache-manifest.4c95d654e479106353db8cbd714802f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e167e6b98c326ff3f2a9618331afa65b",
+    "url": "/react_redux_router_list-of-posts/index.html"
+  },
+  {
+    "revision": "67df9c15c193ffc11e03",
+    "url": "/react_redux_router_list-of-posts/static/css/main.a5b105a4.chunk.css"
+  },
+  {
+    "revision": "4ddb45d97ae67dc5981b",
+    "url": "/react_redux_router_list-of-posts/static/js/2.23c73aa7.chunk.js"
+  },
+  {
+    "revision": "67df9c15c193ffc11e03",
+    "url": "/react_redux_router_list-of-posts/static/js/main.486a2f3a.chunk.js"
+  },
+  {
+    "revision": "c3af7f59abcc33013b36",
+    "url": "/react_redux_router_list-of-posts/static/js/runtime~main.e09aebd2.js"
+  }
+]);
